aa9c695da2974ca53b8ce0017624eb3c19124def
Sala Digital (CtP/ETS) <disrct@bosch.com>
2023-05-09 14:57:08 -0300

    images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navbar';
+import Image from './components/image';
+import { Col, Row } from 'react-bootstrap';
 
 function App() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <section>
+        <Row className='content-products'>
+          <Col md={4} sm={6} xs={10}>
+            <Image />
+          </Col>
+          <Col md={4} sm={6} xs={10}>
+            <Image />
+          </Col>
+          <Col md={4} sm={6} xs={10}>
+            <Image />
+          </Col>
+        </Row>
+      </section>
+    </>
   );
 }
 
---
+++ src/components/image/index.js
@@ -0,0 +1,18 @@
+import { Col } from 'react-bootstrap';
+import './styles.css';
+
+export default function Image(){
+    return (
+        <Col xs={11}   className='content'>
+            <div className='content-image'>
+                <img 
+                    className="image" 
+                    src={'https://blog.coffeemais.com/wp-content/uploads/2022/01/para-que-serve-o-cafe-interna.jpg'} 
+                />
+                <h1 className='title-image'>Café</h1>
+                <p className='value-image'>descrição: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla eu ante quis bibendum. Morbi cursus vestibulum enim a ultrices. Vivamus vitae aliquam risus, facilisis. </p>
+                <p className='value-image'>preço: R$ 4,00</p>
+            </div>
+        </Col>
+    )
+}
